da1254bd086a889d21eca2e790116159fca838c7
Hussein39 <HusseinOjaghi1991@gmail.com>
2018-07-11 14:41:27 +0430

    1) build the project structure. 2) implement the downloadFile method which take the filePath. 3) the downloadFile method checks if the file exist or not. 4) if the file doesn't exists, it will download the file and run apk installer otherwise it will run apk installer.

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
-
-import { NativeModules } from 'react-native';
-
-const { RNFancyDownloader } = NativeModules;
-
-export default RNFancyDownloader;
+import React from 'react';
+import RNFetchBlob from 'react-native-fetch-blob';
+import Styles from './src/style';
+export default class Downloader extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: ''
+        }
+        this.downloadFile = this.downloadFile.bind(this);
+    }
+    downloadFile(filePath) {
+        let downloadTask = null;
+        file_name = RNFetchBlob.fs.dirs.CacheDir + '/com.example.app.apk';
+        RNFetchBlob.fs.exists(file_name)
+            .then((exist) => {
+                if (exist) {
+                    RNFancyDownloader.installApp(file_name);
+                }
+                else {
+                    downloadTask = RNFetchBlob.config({
+                        path: file_name + '.tmp',
+                    })
+                        .fetch('GET', filePath, {});
+                    downloadTask.progress({ interval: 2000 }, (received, total) => {
+                        console.log(parseFloat(received) / parseFloat(total));
+                    })
+                        .then((resp) => {
+                            RNFetchBlob.fs.mv(resp.path(), file_name)
+                                .then(() => {
+                                    RNFancyDownloader.installApp(file_name);
+                                })
+                                .catch((err) => {
+                                    this.setState({ error: err });
+                                });
+                        })
+                        .catch((err) => {
+                            this.setState({ error: err });
+                        });
+                }
+            })
+            .catch((err) => {
+                this.setState({ error: err });
+            });
+    }
+}
