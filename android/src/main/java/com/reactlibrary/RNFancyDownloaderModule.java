2d79de0dcb3c472dc8dac8c5c306cdba597d4bfc
Hussein39 <HusseinOjaghi1991@gmail.com>
2018-07-11 14:18:56 +0430

    1) add installApp method which take an apk Path and open android package installer after change the apk permission 777
    2) delete windows and ios folder because we don't have any
    implementation for them

--- android/src/main/java/com/reactlibrary/RNFancyDownloaderModule.java
+++ android/src/main/java/com/reactlibrary/RNFancyDownloaderModule.java
@@ -5,6 +5,9 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import android.content.Intent;
+import android.net.Uri;
+import java.io.File;
 
 public class RNFancyDownloaderModule extends ReactContextBaseJavaModule {
 
@@ -19,4 +22,22 @@ public class RNFancyDownloaderModule extends ReactContextBaseJavaModule {
   public String getName() {
     return "RNFancyDownloader";
   }
+
+  @ReactMethod
+  public void installApp(String apkPath) {
+    String command = "chmod 777 " + apkPath;
+
+    try {
+      Runtime.getRuntime().exec(command);
+    } catch (Exception error) {
+      error.printStackTrace();
+    }
+
+    Intent openIntent = new Intent(Intent.ACTION_VIEW);
+    openIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    openIntent.setDataAndType(Uri.parse("file://" + apkPath), "application/vnd.android.package-archive");
+
+    reactContext.startActivity(openIntent);
+  }
+
 }
